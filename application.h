05555369738e296c9b885d61624cd8a234b5851e
bezbahen0 <bezbahen0@mail.ru>
2021-04-26 23:38:47 +0300

    upload files

---
+++ application.h
@@ -0,0 +1,75 @@
+
+#ifndef AAPPLICATION_H_
+#define AAPPLICATION_H_
+
+#include <LightGBM/boosting.h>
+#include <LightGBM/dataset.h>
+#include <LightGBM/metric.h>
+#include <LightGBM/objective_function.h>
+#include <LightGBM/prediction_early_stop.h>
+#include <LightGBM/cuda/vector_cudahost.h>
+#include <LightGBM/utils/common.h>
+#include <LightGBM/utils/openmp_wrapper.h>
+#include <LightGBM/utils/text_reader.h>
+#include <LightGBM/config.h>
+#include <LightGBM/meta.h>
+
+#include <memory>
+#include <vector>
+
+namespace LightGBM
+{
+  class ApplicationLightGBM
+  {
+  public:
+    ApplicationLightGBM(int argc, char **argv);
+
+    ~ApplicationLightGBM();
+
+    inline void Run();
+
+  private:
+    void LoadParameters(int argc, char **argv);
+
+    void LoadData();
+
+    void InitTrain();
+
+    void Train();
+
+    void InitPredict();
+
+    void Predict();
+
+    void ConvertModel();
+
+    Config config_;
+    std::unique_ptr<Dataset> train_data_;
+    std::vector<std::unique_ptr<Dataset>> valid_datas_;
+    std::vector<std::unique_ptr<Metric>> train_metric_;
+    std::vector<std::vector<std::unique_ptr<Metric>>> valid_metrics_;
+    std::unique_ptr<Boosting> boosting_;
+    std::unique_ptr<ObjectiveFunction> objective_fun_;
+  };
+
+  inline void ApplicationLightGBM::Run()
+  {
+    if (config_.task == LightGBM::TaskType::kPredict || config_.task == LightGBM::TaskType::KRefitTree)
+    {
+      InitPredict();
+      Predict();
+    }
+    else if (config_.task == LightGBM::TaskType::kConvertModel)
+    {
+      ConvertModel();
+    }
+    else
+    {
+      InitTrain();
+      Train();
+    }
+  }
+
+} // namespace LightGBM
+
+#endif // APPLICATION_H_
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "application.h"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    try
+    {
+        LightGBM::ApplicationLightGBM app(argc, argv);
+        app.Run();
+    }
+    catch (const std::exception &ex)
+    {
+        std::cerr << "Met Exceptions:" << std::endl;
+        std::cerr << ex.what() << std::endl;
+    }
+    catch (const std::string &ex)
+    {
+        std::cerr << "Met Exceptions:" << std::endl;
+        std::cerr << ex << std::endl;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown Exceptions" << std::endl;
+    }
+
+    return 0;
+}
